5f0c6c85620012f9d2ca6b7c14659be21cdc4f9d
ibaker72 <Bakereyad7@gmail.com>
2026-03-14 01:17:39 +0000

    src/lib/config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -5,9 +5,4 @@ export const siteConfig = {
   /** Current availability message shown on the homepage */
   availability: "Accepting Projects",
 
-  /** Quarter label for urgency messaging */
-  currentQuarter: "Q2 2026",
-
-  /** Number of project slots available */
-  openSlots: 2,
 };
